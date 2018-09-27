e0aa4969b326a7d149fa086670a8b9c19d0be9aa
Tim Lines <linest253@yahoo.com>
2018-09-27 14:25:18 -0700

    Minor change to importvgs.sh

--- bin/importvgs.sh
+++ bin/importvgs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in agappsdg1 agappsvg dssdg rewardsdg swvg system techsup1vg techsup2vg vistadg tukvvldap01vg
+for i in agappsdg1 agappsvg dssdg rewardsdg swvg system techsup1vg techsup2vg vistadg
 do
 	vxdg import $i
 	for j in `vxprint -g $i -ht | grep "^v" | awk '{print $2}'`
